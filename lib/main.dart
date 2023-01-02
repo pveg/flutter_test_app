98fc792c20eac1659046704ec090c50774100424
Paulo Torres <ctw02561@Victors-MacBook-Pro.local>
2023-01-02 17:46:29 +0000

    added wordPair and style

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -5,16 +6,18 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+        title: 'Welcome to Flutter',
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Welcome to Flutter'),
+            ),
+            body: Center(
+              child: Text(
+                wordPair.asPascalCase,
+                style: const TextStyle(fontSize: 50),
+              ),
+            )));
   }
 }
